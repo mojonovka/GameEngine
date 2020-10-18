012a5949f3e99cf0aadf67b56e840b699d5962b6
mojonovka <mojonovka@gmail.com>
2020-10-18 22:56:13 +0200

    T5, add coloring using Shaders

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -6,15 +6,16 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
 
 public class MainGameLoop {
 
 	public static void main(String[] args) {
 
 		DisplayManager.createDisplay();
-
 		Loader loader = new Loader();
 		Renderer renderer = new Renderer();
+		StaticShader shader = new StaticShader();
 		
 
 		float[] vertices = {
@@ -33,12 +34,21 @@ public class MainGameLoop {
 		RawModel model = loader.loadToVAO(vertices, indices);
 
 		while (!Display.isCloseRequested()) {
-			renderer.prepare();
+			
 			// game logic
+			
+			renderer.prepare();
+			
+			shader.start();			
+			
 			renderer.render(model);
+			
+			shader.stop();
+			
 			DisplayManager.updateDisplay();
 		}
 
+		shader.cleanUp();
 		loader.cleanUp();
 
 		DisplayManager.closeDisplay();
